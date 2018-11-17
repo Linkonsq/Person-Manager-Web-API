975b5bcfd444a750bcf512f336346390d7524db0
Linkonsq <lsiddique415@gmail.com>
2018-11-17 12:10:02 +0600

    Packages modified

--- PMSApp/App_Start/UnityConfig.cs
+++ PMSApp/App_Start/UnityConfig.cs
@@ -6,21 +6,21 @@ using PMSRepository;
 
 namespace PMSApp
 {
-    public static class UnityConfig
-    {
-        public static void RegisterComponents()
-        {
+	public static class UnityConfig
+	{
+		public static void RegisterComponents()
+		{
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
+			
+			// register all your components with the container here
+			// it is NOT necessary to register your controllers
+			
+			// e.g. container.RegisterType<ITestService, TestService>();
 
-            container.RegisterType<ICategoryRepository, CategoryRepository>();
-            container.RegisterType<IProductRepository, ProductRepository>();
+			container.RegisterType<ICategoryRepository, CategoryRepository>();
+			container.RegisterType<IProductRepository, ProductRepository>();
 
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
-        }
-    }
+			GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+		}
+	}
 }
